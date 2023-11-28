b6bdcbdea466433ffd28af9f5650ec5c45fb7c15
Nicholas Minor <nrminor@wisc.edu>
2023-11-28 12:09:11 -0600

    first commit

---
+++ read_zap_report.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Placeholder module docstring
+"""
+
+
+def main() -> None:
+    """
+    Placeholder main docstring
+    """
+    print("Hello world!")
+
+
+if __name__ == "__main__":
+    main()
